9a75558b4ec7813bcfa884763cf7443d134d2bca
DavideB45 <dadewww@gmail.com>
2024-04-09 18:06:57 +0200

    Done
    the basic structure of the naïve Bayes is done
    but it's still naive
    next step is make it not so much naive

--- NaiveBayes/naiveBaiesModel.py
+++ NaiveBayes/naiveBaiesModel.py
@@ -1,5 +1,6 @@
 from sklearn.naive_bayes import MultinomialNB
 import pandas as pd
+import re
 
 SCHOOLS = ['analytic','aristotle','german_idealism',
            'plato','continental','phenomenology',
@@ -13,11 +14,17 @@ def getData()->pd.DataFrame:
 def splitData(data:pd.DataFrame)->tuple:
     return train_test_split(data['sentence_str'], data['school'], test_size=0.25, random_state=42)
 
-def vectorizeData(x_train:pd.DataFrame, x_test:pd.DataFrame)->tuple:
-    vectorizer = CountVectorizer()
-    x_train = vectorizer.fit_transform(x_train)
-    x_test = vectorizer.transform(x_test)
-    return x_train, x_test
+def addNegationsToken(phrase:str)->str:
+    phrase = re.sub(r"n't", " not", phrase)
+    transformed = re.sub(r'\b(?:not|never|no)\b[\w\s]+', 
+                         lambda match: re.sub(r'(\s+)(\w+)', r'\1NEG_\2', match.group(0)), 
+                         phrase,
+                         flags=re.IGNORECASE)
+    return transformed
+
+def addNegationsToData(data:pd.DataFrame)->pd.DataFrame:
+    data['sentence_str'] = data['sentence_str'].apply(addNegationsToken)
+    return data
 
 if __name__ == '__main__':
     from sklearn.model_selection import train_test_split
@@ -25,19 +32,35 @@ if __name__ == '__main__':
     from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
     import matplotlib.pyplot as plt
     
+    ############################################
+    ######### Beging of the main code  #########
+    ############################################
+
+    #x_train, x_test, y_train, y_test = splitData(addNegationsToData(getData()))
     x_train, x_test, y_train, y_test = splitData(getData())
-    x_train, x_test = vectorizeData(x_train, x_test)
+    vectorizer = CountVectorizer()
+    x_train = vectorizer.fit_transform(x_train)
+    x_test = vectorizer.transform(x_test)
 
     # fitprior (to set whether to learn class prior probabilities or not)
     # alpha (smoothing parameter)
-
-    model = MultinomialNB()
+    model = MultinomialNB(alpha=0.1, fit_prior=True)
     model.fit(x_train, y_train)
     pred = model.predict(x_test)
+
+    ############################################
+    ########### End of the main code ###########
+    ############################################
     
     cm = confusion_matrix(y_test, pred, labels=SCHOOLS, normalize='true')
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=SCHOOLS)
     disp.plot()
     plt.xticks(rotation=45)
     plt.show()
-    
+    print(model.score(x_test, y_test))
+
+    print("give me a sentence: ")
+    sentence = input()
+    sentence = addNegationsToken(sentence)
+    sentence = vectorizer.transform([sentence])
+    print(model.predict(sentence))
