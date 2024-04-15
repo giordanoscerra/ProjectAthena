8ce6064270877a8122297f226f691ff1c55e3037
DavideB45 <dadewww@gmail.com>
2024-04-15 14:10:03 +0200

    removed stopwords
    Performances get worst
    NOT SO NAIVE

--- NaiveBayes/naiveBaiesModel.py
+++ NaiveBayes/naiveBaiesModel.py
@@ -1,6 +1,7 @@
 from sklearn.naive_bayes import MultinomialNB
 import pandas as pd
 import re
+from nltk.corpus import stopwords
 
 SCHOOLS = ['analytic','aristotle','german_idealism',
            'plato','continental','phenomenology',
@@ -13,6 +14,9 @@ def getData()->pd.DataFrame:
 
 def splitData(data:pd.DataFrame)->tuple:
     return train_test_split(data['sentence_str'], data['school'], test_size=0.25, random_state=42)
+
+def getStopWords()->list:
+    return stopwords.words('english')
 
 def addNegationsToken(phrase:str)->str:
     phrase = re.sub(r"n't", " not", phrase)
@@ -38,13 +42,14 @@ if __name__ == '__main__':
 
     #x_train, x_test, y_train, y_test = splitData(addNegationsToData(getData()))
     x_train, x_test, y_train, y_test = splitData(getData())
-    vectorizer = CountVectorizer()
+    vectorizer = CountVectorizer(analyzer='word', stop_words=getStopWords())
+    #vectorizer = CountVectorizer()
     x_train = vectorizer.fit_transform(x_train)
     x_test = vectorizer.transform(x_test)
 
     # fitprior (to set whether to learn class prior probabilities or not)
     # alpha (smoothing parameter)
-    model = MultinomialNB(alpha=0.06, fit_prior=True)
+    model = MultinomialNB(alpha=0.05, fit_prior=True)
     model.fit(x_train, y_train)
     pred = model.predict(x_test)
 
@@ -60,6 +65,7 @@ if __name__ == '__main__':
     print(model.score(x_train, y_train))
     print(model.score(x_test, y_test))
 
+    exit()
     while True:
         print("give me a sentence: ")
         sentence = input()
