4c0664491ec6ac84d81cc13aba7bc371021f9e6c
Chomp97 <yuri.negri2ds@gmail.com>
2024-04-18 15:41:00 +0200

    funziona!!!!

--- rnn/rnn.py
+++ rnn/rnn.py
@@ -11,21 +11,54 @@ class RNN(BaseEstimator):
         self.model = keras.Sequential([
             self.encoder,
             layers.Embedding(
-            input_dim=encoder._output_sequence_length,
-            output_dim=self.hidden_units,
-            # Use masking to handle the variable sequence lengths
-            mask_zero=True),
+            input_dim=len(self.encoder.get_vocabulary()),
+            output_dim=self.hidden_units),
         layers.Bidirectional(layers.LSTM(self.hidden_units)),
         layers.Dense(self.hidden_units, activation='relu'),
         layers.Dense(activation='softmax', units=output_units) # 13 for the number of schools
         ])
-        self.model.compile(loss=keras.losses.CategoricalCrossentropy(from_logits=True),
+        self.model.compile(loss=keras.losses.CategoricalCrossentropy(),
               optimizer=keras.optimizers.Adam(1e-4),
               metrics=['accuracy'])
 
 
-    def fit(self, train_dataset, epochs=10, batch_size=100, validation_data = None):
-        return self.model.fit(train_dataset, epochs=epochs, validation_data = validation_data, batch_size=batch_size)
+    def fit(
+        self,
+        x=None,
+        y=None,
+        batch_size=None,
+        epochs=1,
+        verbose='auto',
+        callbacks=None,
+        validation_split=0.0,
+        validation_data=None,
+        shuffle=True,
+        class_weight=None,
+        sample_weight=None,
+        initial_epoch=0,
+        steps_per_epoch=None,
+        validation_steps=None,
+        validation_batch_size=None,
+        validation_freq=1
+    ):
+        return self.model.fit(
+            x=x,
+            y=y,
+            batch_size=batch_size,
+            epochs=epochs,
+            verbose=verbose,
+            callbacks=callbacks,
+            validation_split=validation_split,
+            validation_data=validation_data,
+            shuffle=shuffle,
+            class_weight=class_weight,
+            sample_weight=sample_weight,
+            initial_epoch=initial_epoch,
+            steps_per_epoch=steps_per_epoch,
+            validation_steps=validation_steps,
+            validation_batch_size=validation_batch_size,
+            validation_freq=validation_freq
+            )
 
 
     def predict(self, X):
--- testrnn.py
+++ testrnn.py
@@ -3,6 +3,11 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from keras.layers import TextVectorization
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import os
 
 def plot_graphs(history, metric):
   plt.plot(history.history[metric])
@@ -11,42 +16,89 @@ def plot_graphs(history, metric):
   plt.ylabel(metric)
   plt.legend([metric, 'val_'+metric])
 
-df = pd.read_csv('philosophy_data.csv')
+save_dir = os.path.join(os.getcwd(), "dset")
+checkpoint_prefix = "checkpoint"
+use_tensorflow_dataset = False
 
-# one-hot encode the schools
-df['school'] = pd.Categorical(df['school'])
+if use_tensorflow_dataset:
+  if (os.path.exists(save_dir)):
+    dataset = tf.data.Dataset.load("dset.dataset")
+  else:
+    df = pd.read_csv('philosophy_data.csv')
+    print("read csv")
 
-dataset = tf.data.Dataset.from_tensor_slices((df['sentence_str'], df['school']))
+    # one-hot encode the schools
+    # df['school'] = pd.Categorical(df['school'])
+
+    dataset_features = df.copy()
+    dset_schools = dataset_features.pop('school')
+    dset_sentences = dataset_features.pop('sentence_str')
+
+    print("got schools and senteces")
+
+    features_dict = {name: np.array(value) for name, value in dset_sentences.items()}
+
+    features_dict = tf.placeholder(tf.string, [None])
+
+    print("created features dict")
+
+    bs = 1024
+    seed = 23
+    dataset = tf.data.Dataset.from_tensor_slices(features_dict)
+
+    print("sliced")
+
+    step_counter = tf.Variable(0, trainable=False)
+    checkpoint_args = {
+      "checkpoint_interval": 50,
+      "step_counter": step_counter,
+      "directory": checkpoint_prefix,
+      "max_to_keep": 20,
+    }
+    dataset.save(dataset, save_dir, checkpoint_args=checkpoint_args)
+
+  print("created dataset")
+  dset_batches = dataset.shuffle(len(dset_schools)).batch(bs)
+else:
+  df = pd.read_csv('philosophy_data.csv')
+  # shuffle the rows of the dataset
+  df = df.sample(frac=1)
+  
+  x_train, x_test, y_train, y_test = train_test_split(df['sentence_str'], df['school'], test_size=0.25, random_state=42)
+  lb_encoder = LabelEncoder()
+  y_train = lb_encoder.fit_transform(y_train)
+  y_train = to_categorical(y_train)
+  y_test = lb_encoder.transform(y_test)
+  y_test = to_categorical(y_test)
 
 # Split the dataset
-train_size = int(0.8 * len(dataset))
-val_size = int(0.1 * len(dataset))
-train = dataset.take(train_size)
-remaining_dataset = dataset.skip(train_size)
-val = remaining_dataset.take(val_size)
-test = remaining_dataset.skip(val_size)
+# train_size = int(0.8 * len(dataset))
+# val_size = int(0.1 * len(dataset))
+# train = dataset.take(train_size)
+# remaining_dataset = dataset.skip(train_size)
+# val = remaining_dataset.take(val_size)
+# test = remaining_dataset.skip(val_size)
 
 # Set vocabulary size
 unique_words = set()
 for sentence in df['sentence_str']:
     unique_words.update(sentence.split())
 
-VOCAB_SIZE = 1000
-SEQ_LENGTH = 100
+print("got unique words")
 
-encoder = tf.keras.layers.TextVectorization(
-    max_tokens=VOCAB_SIZE,
-    output_mode = 'int',
-    output_sequence_length=SEQ_LENGTH)
-encoder.adapt(test.map(lambda sentence_str, label: sentence_str))
+encoder = TextVectorization(max_tokens=len(unique_words), output_mode='int', pad_to_max_tokens=True)
+print("created encoder")
+encoder.adapt(list(unique_words))
+print("adapted encoder")
 
-rnn = RNN(output_units = 13, encoder = encoder, hidden_units = 100)
+rnn = RNN(output_units = 13, encoder = encoder, hidden_units = 20)
+print("created rnn")
 
-history = rnn.fit(train, epochs=10, validation_data=val, batch_size=100)
+history = rnn.fit(x_train, y_train, epochs = 5, validation_split = 0.3)
 
-test_loss, test_acc = rnn.evaluate(test)
-print('Test Loss:', test_loss)
-print('Test Accuracy:', test_acc)
+# test_loss, test_acc = rnn.evaluate(test)
+# print('Test Loss:', test_loss)
+# print('Test Accuracy:', test_acc)
 
 plt.figure(figsize=(16, 8))
 plt.subplot(1, 2, 1)
@@ -58,4 +110,4 @@ plt.ylim(0, None)
 
 sample_text = ('We should eat all landlords')
 print("predicting \"we should eat all landlords\"")
-predictions = rnn.model.predict(np.array([sample_text]))
+predictions = rnn.model.predict(sample_text)
