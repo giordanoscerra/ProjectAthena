00dd886f2b0576e0d9163d7bec203e76d9754df8
DavideB45 <dadewww@gmail.com>
2024-04-19 22:56:03 +0200

    Embedding in model
    Now the embedding is included in the model itself, so we can finetune it.
    Just a problem the last version of TF gives problems and previous versions are not fully optimized for GPU usage i.e. I can't train the model

--- other.py
+++ other.py
@@ -1,61 +1,48 @@
-from sklearn.model_selection import train_test_split
-import pandas as pd
 import numpy as np
 import tensorflow as tf
 from keras import layers
 import keras
 import matplotlib.pyplot as plt
 import tensorflow_hub as hub
-import re
+
+from scoring import *
 
 SCHOOLS = ['analytic','aristotle','german_idealism',
            'plato','continental','phenomenology',
            'rationalism','empiricism','feminism',
            'capitalism','communism','nietzsche',
            'stoicism']
-    
-def getData()->pd.DataFrame:
-    return pd.read_csv('philosophy_data.csv')
 
-def splitData(data:pd.DataFrame)->tuple:
-    return train_test_split(data['sentence_str'], data['school'], test_size=0.25, random_state=42)
 
 print("creating model")
-
 module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
 embed = hub.KerasLayer(module_url)
+print(embed.get_config())
+embed.trainable = True
+print(embed.get_config())
+regularizer = keras.regularizers.l2(0.0001)
 nn = keras.Sequential([
-    keras.Input(shape=(512,)),
-    layers.Dense(256, activation='relu'),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(64, activation='relu'),
-    layers.Dense(13, activation='softmax')
+    keras.Input(shape=[], dtype=tf.string),
+    embed,
+    layers.Dense(256, activation='relu', kernel_regularizer=regularizer),
+    layers.Dense(128, activation='relu', kernel_regularizer=regularizer),
+    layers.Dense(64, activation='relu', kernel_regularizer=regularizer),
+    layers.Dense(13, activation='softmax', kernel_regularizer=regularizer)
 ])
-#define the input as a string passed through the encoder
-def preprocess_input(input_string):
-  encoded_input = embed([input_string])
-  return encoded_input
 
-def preprocess_phrases(phrases):
-  encoded_phrases = []
-  for i, phrase in enumerate(phrases):
-    print(f'processed percentage {i/len(phrases)*100:.2f} %           ', end='\r ')
-    encoded_phrase = preprocess_input(phrase)
-    encoded_phrases.append(encoded_phrase)
-  print('processed percentage 100%        ')
-  return encoded_phrases
-
-nn.compile(loss=keras.losses.CategoricalCrossentropy(from_logits=True),
-        optimizer=keras.optimizers.Adam(1e-4),
-        metrics=['accuracy'])
+print("compiling model")
+nn.compile(loss=keras.losses.CategoricalCrossentropy(),
+  optimizer=keras.optimizers.Adam(0.001),
+  metrics=['accuracy'],
+  )
 
 # Load the dataset
-x_train, x_test, y_train, y_test = splitData(getData())
-print("encoding training data")
-x_train = preprocess_phrases(x_train)
-print("encoding testing data")
-x_test = preprocess_phrases(x_test)
-print("encoding training labels")
+x_train, x_test, y_train, y_test = splitData(filterShortPhrases(getData(), 30))
+#print("encoding training data")
+#x_train = preprocess_phrases(x_train)
+#print("encoding testing data")
+#x_test = preprocess_phrases(x_test)
+#print("encoding training labels")
 #assign each school a number
 y_train = y_train.apply(lambda x: SCHOOLS.index(x))
 y_test = y_test.apply(lambda x: SCHOOLS.index(x))
@@ -68,10 +55,17 @@ x_train = np.array(x_train)
 x_test = np.array(x_test)
 y_train = np.array(y_train)
 y_test = np.array(y_test)
-x_train = x_train.reshape(x_train.shape[0], 512)
-x_test = x_test.reshape(x_test.shape[0], 512)
+#x_train = x_train.reshape(x_train.shape[0], 512)
+#x_test = x_test.reshape(x_test.shape[0], 512)
 print("fitting model")
-history = nn.fit(x_train, y_train, epochs=74, validation_data=(x_test, y_test), batch_size=64)
+#with weight decay
+history = nn.fit(x_train, y_train, 
+         validation_data=(x_test, y_test), 
+         epochs=74, 
+         batch_size=128,
+         verbose=1,
+         callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)],
+        )
 
 print("plotting")
 plt.plot(history.history['accuracy'], label='accuracy')
