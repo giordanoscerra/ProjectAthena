9ad6f2e81695d3ce706abf6ca2cd5271a4487825
DavideB45 <dadewww@gmail.com>
2024-04-20 17:57:34 +0200

    train_test_valid common split

--- dataset_splitting.py
+++ dataset_splitting.py
@@ -3,13 +3,13 @@ import pandas as pd
 from sklearn.model_selection import StratifiedKFold
 
 # Load your datasets
-def getData(validation_size:float=0.1, test_size:float=0.2)-> tuple:
+def getData(validation_size:float=0.2, test_size:float=0.1)-> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     df = pd.read_csv('philosophy_data.csv')
-    development, test = train_test_split(df, test_size=test_size, stratify=df[['school', 'author', 'title']])
+    development, test = train_test_split(df, test_size=test_size, stratify=df['school'], random_state=42, shuffle=True)
     #split vslidation ant train from train
     if validation_size == 0:
         return development, None, test
-    train, validation = train_test_split(development, test_size=validation_size/(1 - test_size), stratify=development[['school', 'author', 'title']])
+    train, validation = train_test_split(development, test_size=validation_size/(1 - test_size), stratify=development['school'], random_state=42, shuffle=True)
     return train, validation, test
 
 def assess_split(train, test):
