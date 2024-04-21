44ce9137a50ae3396f151375cb0822e9f4c78ea8
M4rinz <134485532+M4rinz@users.noreply.github.com>
2024-04-21 20:53:22 +0200

    Sketched hyperparameter selection. But I think it's wrong. I dunno

--- NaiveBayes/NB_Tf-Idf.py
+++ NaiveBayes/NB_Tf-Idf.py
@@ -10,9 +10,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from scoring import scorePhilosophy
 from utilities import getData
 
+from sklearn.metrics import precision_recall_fscore_support, f1_score
 
-# import data
-df = pd.read_csv('philosophy_data.csv')
 
 # data splitting
 tr, vl, _ = getData()
@@ -22,8 +21,8 @@ y_train = tr['school']
 X_val = vl['sentence_str']
 y_val = vl['school']
 
-# declare the "builder" of the Tf-Idf weighted
-# term-document matrix
+
+# declare the "builder" of the Tf-Idf weighted term-document matrix
 vectorizer = TfidfVectorizer()
 # create the vocabulary and obtain the document-term matrix
 # for the training data...
@@ -31,16 +30,37 @@ X_train = vectorizer.fit_transform(X_train)
 # ... and "test" data (without changing the vocabulary)
 X_val = vectorizer.transform(X_val)
 
-# I copied the alpha from DavideB. maybe can be fine-tuned with 
-# a model selection approach...
-model = MultinomialNB(alpha=0.06, fit_prior=True)
-model.fit(X_train, y_train)
-y_pred = model.predict(X_val)
+smoothing = 1/np.logspace(0,3,num=4) #0,4,num=10
+pars_results = []
+for smooth_par in smoothing:
+    model = MultinomialNB(alpha=smooth_par, fit_prior=True)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_val)
+    print()
+    print(f'\tSmoothing par = {smooth_par}')
+    print('Macroaverage:')
+    print(precision_recall_fscore_support(y_val, y_pred, zero_division=0.0, average='macro'))
+    print('Microaverage:')
+    print(precision_recall_fscore_support(y_val, y_pred, zero_division=0.0, average='micro'))
+    print('Weighted Macroaverage??')
+    print(precision_recall_fscore_support(y_val, y_pred, zero_division=0.0, average='weighted'))
+
+    # I choose as a reference metric the microaverage F1
+    pars_results.append((f1_score(y_true=y_val, y_pred=y_pred, average='micro'),smooth_par))
+
+# Let's look for the best
+best_f1, best_alpha = sorted(pars_results)[-1]
+print(f'Found that best smoothing parameter is {best_alpha}')
+print(f'Which gave a F1 score on the validation set of {best_f1}')
+
+# we can retrain now. Shall we do it?
+
+
 
 #SCHOOLS = df['school'].unique()
 
-scorePhilosophy(prediction=y_pred, 
-                ground_truth=y_val,
-                showConfusionMatrix=True,
-                saveFolder='NaiveBayes/Images',
-                saveName='NB_TF-Idf')
+# scorePhilosophy(prediction=y_pred, 
+#                 ground_truth=y_val,
+#                 showConfusionMatrix=True,
+#                 saveFolder='NaiveBayes/Images',
+#                 saveName='NB_TF-Idf')
