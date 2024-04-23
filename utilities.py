97cd017249a13f393795847eab81ec7fdad96b8d
DavideB45 <dadewww@gmail.com>
2024-04-23 14:50:23 +0200

    tuple

--- utilities.py
+++ utilities.py
@@ -1,8 +1,9 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
+from typing import Tuple
 
 # Load your datasets
-def getData(validation_size:float=0.2, test_size:float=0.1)-> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+def getData(validation_size:float=0.2, test_size:float=0.1)-> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     df = pd.read_csv('philosophy_data.csv')
     development, test = train_test_split(df, test_size=test_size, stratify=df['school'], random_state=42, shuffle=True)
     #split vslidation ant train from train
