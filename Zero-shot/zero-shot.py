0fe55cea45703b55f36d6160f14c6e2b96a782f1
Davide <davidemarchi00@gmail.com>
2024-05-18 23:16:17 +0200

    Dummy to test progress bar and execution

---
+++ Zero-shot/zero-shot.py
@@ -0,0 +1,80 @@
+import os
+import sys
+import json
+import numpy as np
+from tqdm import tqdm
+from transformers import pipeline
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from scoring import scorePhilosophy, SCHOOLS  # Assuming SCHOOLS is a list of school names
+from utilities import getData
+
+# Load data
+_, vl, _ = getData(min_chars=20, max_chars=1700)
+texts_vl = vl['sentence_str'].tolist()[:15]
+labels_vl = vl['school'].tolist()[:15]
+texts_vl_subsets = np.array_split(texts_vl, 10)
+labels_vl_subsets = np.array_split(labels_vl, 10)
+
+# Model name and path
+model_name = 'deberta-v3-small-tasksource-nli'
+#model_name = 'bart-large-mnli'
+model_path = '../' + model_name
+classifier = pipeline("zero-shot-classification", model=model_path)
+
+# Initialize an empty list to store the predictions
+predictions = []
+
+# Process each part separately
+for subset in tqdm(texts_vl_subsets, desc="Processing subsets"):
+    if len(subset) > 0:
+        predictions.extend(classifier(subset.tolist(), SCHOOLS))
+
+print(predictions)
+print(len(predictions))
+#print(predictions[0])
+#print([pred['labels'][np.argmax(pred['scores'])] for pred in predictions])
+print('-----------------------------------')
+preds = classifier(texts_vl, SCHOOLS)
+print(preds)
+print(len(preds))
+#print(preds[0])
+#print([pred['labels'][np.argmax(pred['scores'])] for pred in preds])
+
+# Check if predictions and preds contain the same things
+if predictions == preds:
+    print("predictions and preds contain the same things")
+else:
+    print("predictions and preds do not contain the same things")
+
+
+exit()
+# Get the predicted labels
+predicted_labels = [pred['labels'][np.argmax(pred['scores'])] for pred in predictions]
+
+# Create a dictionary with the report, accuracy, and confusion matrix
+results = {
+
+    # Calculate accuracy
+    'accuracy': accuracy_score(labels_vl, predicted_labels),
+
+    # Generate classification report
+    'report': classification_report(labels_vl, predicted_labels, labels=SCHOOLS, output_dict=True, zero_division=1),
+
+    # Generate confusion matrix
+    'confusion_matrix': confusion_matrix(labels_vl, predicted_labels, labels=SCHOOLS).tolist()  # Convert the numpy array to a list
+}
+
+# Write the JSON string to a file
+with open('Zero-shot/results.json', 'w') as f:
+
+    # Convert the dictionary to a JSON string
+    json.dump(results, f, indent=4)
+
+# Plot the confusion matrix
+scorePhilosophy(predicted_labels, labels_vl, modelName='Zero-shot', subtitle='Zero-shot Classification', saveFolder= 'Zero-shot', saveName='zero-shot')
+
+#{'labels': ['travel', 'dancing', 'cooking'],
+# 'scores': [0.9938651323318481, 0.0032737774308770895, 0.002861034357920289],
+# 'sequence': 'one day I will see the world'}
