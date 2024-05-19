d9f084f0bf7197acdd9d0d8f57d2797f570ea20d
DavideB45 <dadewww@gmail.com>
2024-05-19 13:29:30 +0200

    distilBert only training

--- distilBert/all_in_one_file.py
+++ distilBert/all_in_one_file.py
@@ -12,18 +12,19 @@ from scoring import SCHOOLS
 from utilities import Logger, getData
 
 # Load data
-tr, vl, _ = getData(min_chars=300, max_chars=1700)
-batchSize = 30
+tr, vl, _ = getData(min_chars=200, max_chars=1700)
+batchSize = 50
 num_epochs = 3
 learning_rate = 2e-5
 print(f'training quantity: {len(tr)}', f'validation quantity: {len(vl)}')
 texts_tr = tr['sentence_str']
 labels_tr = tr['school']
-texts_vl = vl['sentence_str']
-labels_vl = vl['school']
+#texts_vl = vl['sentence_str']
+#labels_vl = vl['school']
+
 
 labels_tr = torch.tensor([SCHOOLS.index(label) for label in labels_tr])
-labels_vl = torch.tensor([SCHOOLS.index(label) for label in labels_vl])
+#labels_vl = torch.tensor([SCHOOLS.index(label) for label in labels_vl])
 
 model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased', num_labels=len(SCHOOLS))
 tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
@@ -39,14 +40,14 @@ dataset = TensorDataset(input_ids, attention_texts, labels_tr)
 dataloader_tr = DataLoader(dataset, batch_size=batchSize, shuffle=True)
 
 print('tokenizing validation...')
-texts_vl = texts_vl.tolist()
-tok = tokenizer(texts_vl, add_special_tokens=True, padding='max_length', max_length=280, truncation=True)
-tokenized_texts_vl = tok['input_ids']
-attention_texts_vl = tok['attention_mask']
-input_ids_vl = torch.tensor(tokenized_texts_vl)
-attention_texts_vl = torch.tensor(attention_texts_vl)
-dataset_vl = TensorDataset(input_ids_vl, attention_texts_vl, labels_vl)
-dataloader_vl = DataLoader(dataset_vl, batch_size=batchSize, shuffle=True)
+#texts_vl = texts_vl.tolist()
+#tok = tokenizer(texts_vl, add_special_tokens=True, padding='max_length', max_length=280, truncation=True)
+#tokenized_texts_vl = tok['input_ids']
+#attention_texts_vl = tok['attention_mask']
+#input_ids_vl = torch.tensor(tokenized_texts_vl)
+#attention_texts_vl = torch.tensor(attention_texts_vl)
+#dataset_vl = TensorDataset(input_ids_vl, attention_texts_vl, labels_vl)
+#dataloader_vl = DataLoader(dataset_vl, batch_size=batchSize, shuffle=True)
 
 device = torch.device("cuda" if torch.cuda.is_available()
                       else "mps" if torch.backends.mps.is_available()
@@ -79,9 +80,9 @@ def compute_epoch(model:DistilBertForSequenceClassification, dataloader, optimiz
         now = time.time()
         eta = (now - begin) * (len(dataloader) - batchIndex) / batchIndex
         print(f'Epoch: {epoch}, Loss: {loss.item():.4f}, accuracy: {batch_accuracy:.2f}     eta = {int(eta//60):03d}m {int(eta%60):02d}s      ', end='\r')
-        del input_ids, labels, outputs, logits, loss
-        gc.collect()
-        torch.cuda.empty_cache()
+        #del input_ids, labels, outputs, logits, loss
+        #gc.collect()
+        #torch.cuda.empty_cache()
     return total_loss/batchIndex, total_accuracy/batchIndex
 
 start_time = datetime.now()
@@ -96,9 +97,9 @@ for epoch in range(num_epochs):
     model.train()
     loss,acc = compute_epoch(model, dataloader_tr, optimizer, epoch=epoch, device=device)
     logger.add(f'Epoch: {epoch}, TR Loss: {loss:.4f}, TR accuracy: {acc:.2f}')
-    model.eval()
-    loss,acc = compute_epoch(model, dataloader_vl, optimizer, backpropagate=False, epoch=epoch, device=device)
-    logger.add(f'Epoch: {epoch}, VL Loss: {loss:.4f}, VL accuracy: {acc:.2f}')
+    #model.eval()
+    #loss,acc = compute_epoch(model, dataloader_vl, optimizer, backpropagate=False, epoch=epoch, device=device)
+    #logger.add(f'Epoch: {epoch}, VL Loss: {loss:.4f}, VL accuracy: {acc:.2f}')
     model.save_pretrained(os.path.join(sys.path[0], 'tuned',f'bert_model_{epoch}'))
     tokenizer.save_pretrained(os.path.join(sys.path[0], 'tuned',f'bert_tokenizer_{epoch}'))
 
