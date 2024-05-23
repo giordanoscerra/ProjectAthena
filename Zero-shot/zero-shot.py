ca85b0b661484bc2f86108f08da92b48782a414d
Davide <davidemarchi00@gmail.com>
2024-05-23 12:10:33 +0200

    Zero shot execute on new labels 84+ chars bart-large-mnli

--- Zero-shot/zero-shot.py
+++ Zero-shot/zero-shot.py
@@ -2,8 +2,8 @@ import os
 import sys
 import json
 import torch
-import numpy as np
-from tqdm import tqdm
+# import numpy as np
+# from tqdm import tqdm
 from transformers import pipeline
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
@@ -11,84 +11,97 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from scoring import scorePhilosophy, SCHOOLS  # Assuming SCHOOLS is a list of school names
 from utilities import getData
 
-renamed_schools = ['Analytic Philosophy',
-                   'Aristotelian Philosophy',
-                   'German Idealism Philosophy',
-                   'Platonic Philosophy',
-                    'Continental Philosophy',
-                    'Phenomenological Philosophy',
-                    'Rationalist Philosophy',
-                    'Empiricist Philosophy',
-                    'Feminist Philosophy',
-                    'Capitalist Philosophy',
-                    'Communist Philosophy',
-                    'Nietzschean Philosophy',
-                    'Stoic Philosophy'
-                    ]
+def zero_shot():
 
-# Check if CUDA is available
-device = 'cuda:1' if torch.cuda.is_available() else -1  # set device to first GPU, -1 for CPU
+    labels_dict = {
+        'Analytic Philosophy': 'analytic',
+        'Aristotelian Philosophy': 'aristotle',
+        'German Idealism Philosophy': 'german_idealism',
+        'Platonic Philosophy': 'plato',
+        'Continental Philosophy': 'continental',
+        'Phenomenological Philosophy': 'phenomenology',
+        'Rationalist Philosophy': 'rationalism',
+        'Empiricist Philosophy': 'empiricism',
+        'Feminist Philosophy': 'feminism',
+        'Capitalist Philosophy': 'capitalism',
+        'Communist Philosophy': 'communism',
+        'Nietzschean Philosophy': 'nietzsche',
+        'Stoic Philosophy': 'stoicism'
+    }
 
-# Load data
-min_chars = 84
-_, vl, _ = getData(min_chars=min_chars)
-texts_vl = vl['sentence_str'].tolist()
-labels_vl = vl['school'].tolist()
-#texts_vl_subsets = np.array_split(texts_vl, 10)
-#labels_vl_subsets = np.array_split(labels_vl, 10)
+    # Check if all strings in SCHOOLS exist as keys in labels_dict
+    if all(label in labels_dict.values() for label in SCHOOLS) and len(labels_dict) == len(SCHOOLS):
+        print("All strings in SCHOOLS exist as values in labels_dict")
+    else:
+        print("Not all strings in SCHOOLS exist as values in labels_dict")
+        return None
 
-# Model name and path
-#model_name = 'deberta-v3-small-tasksource-nli'
-#model_name = 'bart-large-mnli'
-#model_path = '../' + model_name
-classifier = pipeline("zero-shot-classification", model='facebook/bart-large-mnli', device=device)
+    # Check if CUDA is available
+    device = 'cuda' if torch.cuda.is_available() else -1  # set device to first GPU, -1 for CPU
 
-"""
-# Initialize an empty list to store the predictions
-predictions = []
+    # Load data
+    min_chars = 84
+    _, vl, _ = getData(min_chars=min_chars)
+    texts_vl = vl['sentence_str'].tolist()
+    labels_vl = vl['school'].tolist()
+    #texts_vl_subsets = np.array_split(texts_vl, 10)
+    #labels_vl_subsets = np.array_split(labels_vl, 10)
 
-# Process each part separately
-for subset in tqdm(texts_vl_subsets, desc="Processing subsets"):
-    if len(subset) > 0:
-        predictions.extend(classifier(subset.tolist(), SCHOOLS))
-"""
+    # Model name and path
+    #model_name = 'deberta-v3-small-tasksource-nli'
+    #model_name = 'bart-large-mnli'
+    #model_path = '../' + model_name
+    classifier = pipeline("zero-shot-classification", model='facebook/bart-large-mnli', device=device)
 
-predictions = classifier(texts_vl, renamed_schools)
+    """
+    # Initialize an empty list to store the predictions
+    predictions = []
 
-# Save predictions in an indented JSON file
-with open('Zero-shot/predictions.json', 'w') as f:
-    
-    # Convert the predictions list to a JSON string with indentation
-    json.dump(predictions, f, indent=4)
+    # Process each part separately
+    for subset in tqdm(texts_vl_subsets, desc="Processing subsets"):
+        if len(subset) > 0:
+            predictions.extend(classifier(subset.tolist(), SCHOOLS))
+    """
 
-# Get the predicted labels
-predicted_labels = [pred['labels'][0] for pred in predictions]
+    predictions = classifier(texts_vl, labels_dict.keys())
 
-# Create a dictionary with the report, accuracy, and confusion matrix
-results = {
+    # Save predictions in an indented JSON file
+    with open('Zero-shot/predictions.json', 'w') as f:
+        
+        # Convert the predictions list to a JSON string with indentation
+        json.dump(predictions, f, indent=4)
 
-    # Minimum number of characters per sentence
-    'min_chars': min_chars,
+    # Get the predicted labels
+    predicted_labels = [labels_dict[pred['labels'][0]] for pred in predictions]
 
-    # Calculate accuracy
-    'accuracy': accuracy_score(labels_vl, predicted_labels),
+    # Create a dictionary with the report, accuracy, and confusion matrix
+    results = {
 
-    # Generate classification report
-    'report': classification_report(labels_vl, predicted_labels, labels=SCHOOLS, output_dict=True, zero_division=1),
+        # Minimum number of characters per sentence
+        'min_chars': min_chars,
 
-    # Generate confusion matrix
-    'confusion_matrix': confusion_matrix(labels_vl, predicted_labels, labels=SCHOOLS).tolist()  # Convert the numpy array to a list
-}
+        # Calculate accuracy
+        'accuracy': accuracy_score(labels_vl, predicted_labels),
 
-# Write the JSON string to a file
-with open('Zero-shot/results.json', 'w') as f:
+        # Generate classification report
+        'report': classification_report(labels_vl, predicted_labels, labels=SCHOOLS, output_dict=True, zero_division=1),
 
-    # Convert the dictionary to a JSON string
-    json.dump(results, f, indent=4)
+        # Generate confusion matrix
+        'confusion_matrix': confusion_matrix(labels_vl, predicted_labels, labels=SCHOOLS).tolist()  # Convert the numpy array to a list
+    }
 
-# Plot the confusion matrix
-scorePhilosophy(predicted_labels, labels_vl, modelName='Zero-shot', subtitle='Zero-shot Classification', saveFolder= 'Zero-shot', saveName='zero-shot')
+    # Write the JSON string to a file
+    with open('Zero-shot/results.json', 'w') as f:
 
-#{'labels': ['travel', 'dancing', 'cooking'],
-# 'scores': [0.9938651323318481, 0.0032737774308770895, 0.002861034357920289],
-# 'sequence': 'one day I will see the world'}
+        # Convert the dictionary to a JSON string
+        json.dump(results, f, indent=4)
+
+    # Plot the confusion matrix
+    scorePhilosophy(predicted_labels, labels_vl, modelName='Zero-shot', subtitle='Zero-shot Classification', saveFolder= 'Zero-shot', saveName='zero-shot')
+
+    #{'labels': ['travel', 'dancing', 'cooking'],
+    # 'scores': [0.9938651323318481, 0.0032737774308770895, 0.002861034357920289],
+    # 'sequence': 'one day I will see the world'}
+
+if __name__ == '__main__':
+    zero_shot()
