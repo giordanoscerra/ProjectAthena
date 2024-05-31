52c62e11647933227454c7af32bbcb1b80495963
Davide <davidemarchi00@gmail.com>
2024-05-31 16:24:27 +0200

    Changed zero-shot

--- Zero-shot/zero-shot.py
+++ Zero-shot/zero-shot.py
@@ -11,23 +11,10 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from scoring import scorePhilosophy, SCHOOLS  # Assuming SCHOOLS is a list of school names
 from utilities import getData
 
-def zero_shot():
+def zero_shot(labels_dict:dict=None, min_chars:int=None, max_chars:int=None, folder:str='results/'):
 
-    labels_dict = {
-        'Analytic Philosophy': 'analytic',
-        'Aristotelian Philosophy': 'aristotle',
-        'German Idealism Philosophy': 'german_idealism',
-        'Platonic Philosophy': 'plato',
-        'Continental Philosophy': 'continental',
-        'Phenomenological Philosophy': 'phenomenology',
-        'Rationalist Philosophy': 'rationalism',
-        'Empiricist Philosophy': 'empiricism',
-        'Feminist Philosophy': 'feminism',
-        'Capitalist Philosophy': 'capitalism',
-        'Communist Philosophy': 'communism',
-        'Nietzschean Philosophy': 'nietzsche',
-        'Stoic Philosophy': 'stoicism'
-    }
+    if labels_dict is None:
+        labels_dict = {label: label for label in SCHOOLS}
 
     # Check if all strings in SCHOOLS exist as keys in labels_dict
     if all(label in labels_dict.values() for label in SCHOOLS) and len(labels_dict) == len(SCHOOLS):
@@ -40,8 +27,7 @@ def zero_shot():
     device = 'cuda' if torch.cuda.is_available() else -1  # set device to first GPU, -1 for CPU
 
     # Load data
-    min_chars = 84
-    _, vl, _ = getData(min_chars=min_chars)
+    _, vl, _ = getData(min_chars=min_chars, max_chars=max_chars)
     texts_vl = vl['sentence_str'].tolist()
     labels_vl = vl['school'].tolist()
     #texts_vl_subsets = np.array_split(texts_vl, 10)
@@ -63,11 +49,12 @@ def zero_shot():
             predictions.extend(classifier(subset.tolist(), SCHOOLS))
     """
 
-    predictions = classifier(texts_vl, labels_dict.keys())
+    predictions = classifier(texts_vl, list(labels_dict.keys()))
 
     # Save predictions in an indented JSON file
-    with open('Zero-shot/predictions.json', 'w') as f:
-        
+    os.makedirs('Zero-shot/' + folder, exist_ok=True)
+    with open('Zero-shot/' + folder + 'predictions.json', 'w') as f:
+
         # Convert the predictions list to a JSON string with indentation
         json.dump(predictions, f, indent=4)
 
@@ -91,17 +78,35 @@ def zero_shot():
     }
 
     # Write the JSON string to a file
-    with open('Zero-shot/results.json', 'w') as f:
+    with open('Zero-shot/' + folder + 'results.json', 'w') as f:
 
         # Convert the dictionary to a JSON string
         json.dump(results, f, indent=4)
 
     # Plot the confusion matrix
-    scorePhilosophy(predicted_labels, labels_vl, modelName='Zero-shot', subtitle='Zero-shot Classification', saveFolder= 'Zero-shot', saveName='zero-shot')
+    scorePhilosophy(predicted_labels, labels_vl, modelName='Zero-shot', subtitle='Zero-shot Classification', saveFolder= 'Zero-shot/'+ folder, saveName='zero-shot')
 
     #{'labels': ['travel', 'dancing', 'cooking'],
     # 'scores': [0.9938651323318481, 0.0032737774308770895, 0.002861034357920289],
     # 'sequence': 'one day I will see the world'}
 
 if __name__ == '__main__':
-    zero_shot()
+
+    labels_dict = {
+        'Analytic Philosophy': 'analytic',
+        'Aristotelian Philosophy': 'aristotle',
+        'German Idealism Philosophy': 'german_idealism',
+        'Platonic Philosophy': 'plato',
+        'Continental Philosophy': 'continental',
+        'Phenomenological Philosophy': 'phenomenology',
+        'Rationalist Philosophy': 'rationalism',
+        'Empiricist Philosophy': 'empiricism',
+        'Feminist Philosophy': 'feminism',
+        'Capitalist Philosophy': 'capitalism',
+        'Communist Philosophy': 'communism',
+        'Nietzschean Philosophy': 'nietzsche',
+        'Stoic Philosophy': 'stoicism'
+    }
+
+    zero_shot(max_chars=83, folder='bart-large-mnli_83-_chars_noLabels/')
+    zero_shot(labels_dict=labels_dict, max_chars=83, folder='bart-large-mnli_83-_chars_phiLabels/')
