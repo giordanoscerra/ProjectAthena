f84477d02662013c88a360570f230e007c6d33b4
DavideB45 <dadewww@gmail.com>
2024-06-02 14:20:27 +0200

    Naive bayes prettier

--- NaiveBayes/NB_GridSearch.py
+++ NaiveBayes/NB_GridSearch.py
@@ -15,7 +15,7 @@ from scoring import scorePhilosophy
 from utilities import getData
 
 method = 'tfidf'  # 'count' or 'tfidf'
-
+saveResults = True # True if you want to save the results in a file
 # file, folder, path... Where to record our numbers
 # Pathing is a nightmare in python...
 FOLDER = os.path.join('NaiveBayes','Results')
@@ -43,11 +43,11 @@ X_train = vectorizer.fit_transform(X_train)
 # ... and "test" data (without changing the vocabulary)
 X_val = vectorizer.transform(X_val)
 
-
-with open(FILEPATH, 'w') as file:
-    file.write('Naive Bayes on the '+ method +' weighted term-document matrix\n')
-    file.write('Scores for different values of the smoothing parameter\n\n')
-    file.write(f'Ran on {datetime.now()}\n\n')
+if saveResults:
+    with open(FILEPATH, 'w') as file:
+        file.write('Naive Bayes on the '+ method +' weighted term-document matrix\n')
+        file.write('Scores for different values of the smoothing parameter\n\n')
+        file.write(f'Ran on {datetime.now()}\n\n')
 
 
 # declare the parameters for grid search
@@ -62,25 +62,26 @@ for parameters in ParameterGrid(full_grid):
     model.fit(X_train, y_train)
     y_pred = model.predict(X_val)
 
-    with open(FILEPATH, 'a') as file:
-        for key, value in parameters.items():
-            rest = f'{value}\t' if isinstance(value,bool) else f'{value:<10.6f}\t'
-            file.write(f'\t{key} = ' + rest)
-        file.write('\n')
+    if saveResults:
+        with open(FILEPATH, 'a') as file:
+            for key, value in parameters.items():
+                rest = f'{value}\t' if isinstance(value,bool) else f'{value:<10.6f}\t'
+                file.write(f'\t{key} = ' + rest)
+            file.write('\n')
 
-        macroPrec, macroRec, macroF1, _ = precision_recall_fscore_support(y_val, y_pred, zero_division=0.0, average='macro')
-        file.write(f'Macroaverage precision = {macroPrec:<10.5f}\t') 
-        file.write(f'Macroaverage recall = {macroRec:<10.5f}\t')
-        file.write(f'Macroaverage F1 = {macroF1:<10.5f}\n')
+            macroPrec, macroRec, macroF1, _ = precision_recall_fscore_support(y_val, y_pred, zero_division=0.0, average='macro')
+            file.write(f'Macroaverage precision = {macroPrec:<10.5f}\t') 
+            file.write(f'Macroaverage recall = {macroRec:<10.5f}\t')
+            file.write(f'Macroaverage F1 = {macroF1:<10.5f}\n')
 
-        weightedPrec, weightedRec, weightedF1, _ = precision_recall_fscore_support(y_val, y_pred, zero_division=0.0, average='weighted')
-        file.write(f'Macroaverage precision = {weightedPrec:<10.5f}\t')
-        file.write(f'Macroaverage recall = {weightedRec:<10.5f}\t')
-        file.write(f'Macroaverage F1 = {weightedF1:<10.5f}\n')
+            weightedPrec, weightedRec, weightedF1, _ = precision_recall_fscore_support(y_val, y_pred, zero_division=0.0, average='weighted')
+            file.write(f'Macroaverage precision = {weightedPrec:<10.5f}\t')
+            file.write(f'Macroaverage recall = {weightedRec:<10.5f}\t')
+            file.write(f'Macroaverage F1 = {weightedF1:<10.5f}\n')
 
-        micro = f1_score(y_true=y_val, y_pred=y_pred, average='micro')
-        file.write(f'Microaverage precision = microaverage recall = microaverage F1 = {micro:<10.5f}\n')
-        file.write('\n')
+            micro = f1_score(y_true=y_val, y_pred=y_pred, average='micro')
+            file.write(f'Microaverage precision = microaverage recall = microaverage F1 = {micro:<10.5f}\n')
+            file.write('\n')
 
     # I choose as a reference metric the microaverage F1
     pars_results.append((f1_score(y_true=y_val, y_pred=y_pred, average='micro'),parameters))
@@ -88,14 +89,15 @@ for parameters in ParameterGrid(full_grid):
 # Let's look for the best
 best_f1, best_parameters = sorted(pars_results)[-1]
 
-# print the best parameters on file
-with open(FILEPATH, 'a') as file:
-    file.write('\n\n\t\tBest parameters found:\n')
-    for key, value in best_parameters.items():
-        rest = f'{value}\t' if isinstance(value,bool) else f'{value:<10.6f}\t'
-        file.write(f'\t{key} = ' + rest)
-    file.write('\n')
-    file.write(f'F1 score on the validation set = {best_f1:.4f}\n')
+if saveResults:
+    # print the best parameters on file
+    with open(FILEPATH, 'a') as file:
+        file.write('\n\n\t\tBest parameters found:\n')
+        for key, value in best_parameters.items():
+            rest = f'{value}\t' if isinstance(value,bool) else f'{value:<10.6f}\t'
+            file.write(f'\t{key} = ' + rest)
+        file.write('\n')
+        file.write(f'F1 score on the validation set = {best_f1:.4f}\n')
 
 # print the best parameters on screen
 print('Found that best parameters are: ')
@@ -115,7 +117,7 @@ report = scorePhilosophy(prediction=y_pred,
                 subtitle=f'alpha = {best_parameters["alpha"]:.4f}, fit_prior = {best_parameters["fit_prior"]}',
                 showConfusionMatrix=True,
                 saveFolder='NaiveBayes/Images',
-                saveName='NB_'+method)
-
-with open(FILEPATH, 'a') as file:
-    file.write(report)
+                saveName='NB_'+method if saveResults else None)
+if saveResults:
+    with open(FILEPATH, 'a') as file:
+        file.write(report)
