cf36410638266bad1449652bf0d3fda905c5238f
DavideB45 <dadewww@gmail.com>
2024-06-02 15:13:06 +0200

    celanup + coupple comments

--- distilBert/all_in_one_file.py
+++ distilBert/all_in_one_file.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-import gc
 import os
 import sys
 import time
@@ -16,7 +15,6 @@ min_chars = None # or could be None
 save_string = 'min_chars_'+ str(min_chars) if min_chars is not None else 'full_dataset'
 
 # Load data
-#tr, vl, _ = getData(min_chars=200, max_chars=1700)
 tr, vl, _ = getData(min_chars=min_chars)
 batchSize = 50
 num_epochs = 3
@@ -26,14 +24,14 @@ texts_tr = tr['sentence_str']
 labels_tr = tr['school']
 texts_vl = vl['sentence_str']
 labels_vl = vl['school']
-
-
 labels_tr = torch.tensor([SCHOOLS.index(label) for label in labels_tr])
 labels_vl = torch.tensor([SCHOOLS.index(label) for label in labels_vl])
 
+# Load DistilBERT model and tokenizer
 model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased', num_labels=len(SCHOOLS))
 tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
 
+# Tokenize the training data
 print('tokenizing training...')
 texts_tr = texts_tr.tolist()
 tok = tokenizer(texts_tr, add_special_tokens=True, padding='max_length', max_length=512, truncation=True)
@@ -44,6 +42,7 @@ attention_texts = torch.tensor(attention_texts)
 dataset = TensorDataset(input_ids, attention_texts, labels_tr)
 dataloader_tr = DataLoader(dataset, batch_size=batchSize, shuffle=True)
 
+# Tokenize the validation data
 print('tokenizing validation...')
 texts_vl = texts_vl.tolist()
 tok = tokenizer(texts_vl, add_special_tokens=True, padding='max_length', max_length=512, truncation=True)
@@ -54,11 +53,13 @@ attention_texts_vl = torch.tensor(attention_texts_vl)
 dataset_vl = TensorDataset(input_ids_vl, attention_texts_vl, labels_vl)
 dataloader_vl = DataLoader(dataset_vl, batch_size=batchSize, shuffle=True)
 
+# set device
 device = torch.device("cuda:1")
 print('device is:',device)
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 model.to(device)
 
+# Training epoch
 def compute_epoch(model:DistilBertForSequenceClassification, dataloader, optimizer, criterion=nn.functional.cross_entropy, backpropagate=True, epoch=0, device=None) -> tuple[float, float, str]:
     batchIndex = 0
     total_loss = 0
@@ -91,29 +92,24 @@ def compute_epoch(model:DistilBertForSequenceClassification, dataloader, optimiz
     report = classification_report(all_labels, all_predictions)
     return total_loss/batchIndex, total_accuracy/batchIndex, report
 
+# Logger
 start_time = datetime.now()
 print('Training and Validation -> Start Time:', start_time.strftime("H%M%S"))
 log_path = os.path.join(sys.path[0], 'log_' + datetime.now().strftime("%Y%m%d%H%M%S") + '.txt')
 logger = Logger(log_path)
 logger.add("Training and Validation -> Start Time: " + start_time.strftime("H%M%S"))
-
+# Training loop
 for epoch in range(num_epochs):
     print(f'\nEpoch {epoch+1}/{num_epochs}')
     logger.add(f'Epoch {epoch+1}/{num_epochs}')
     model.train()
     loss,acc,_ = compute_epoch(model, dataloader_tr, optimizer, epoch=epoch, device=device)
     logger.add(f'Epoch: {epoch}, TR Loss: {loss:.4f}, TR accuracy: {acc:.2f}')
-    #model.eval()
-    #loss,acc = compute_epoch(model, dataloader_vl, optimizer, backpropagate=False, epoch=epoch, device=device)
-    #logger.add(f'Epoch: {epoch}, VL Loss: {loss:.4f}, VL accuracy: {acc:.2f}')
     model.save_pretrained(os.path.join(sys.path[0], 'tuned',f'bert_model_{epoch}_{save_string}'))
     tokenizer.save_pretrained(os.path.join(sys.path[0], 'tuned',f'bert_tokenizer_{epoch}_{save_string}'))
-
 end_time = datetime.now()
 
-model.save_pretrained(os.path.join(sys.path[0], 'tuned',f'final_bert_model_{datetime.now().strftime("%d%H%M")}_{save_string}'))
-tokenizer.save_pretrained(os.path.join(sys.path[0], 'tuned',f'final_bert_tokenizer_{datetime.now().strftime("%d%H%M")}_{save_string}'))
-
+# Validation
 model.eval()
 loss,acc,report = compute_epoch(model, dataloader_vl, optimizer, backpropagate=False, epoch=epoch, device=device)
 logger.add(f'Epoch: {epoch}, VL Loss: {loss:.4f}, VL accuracy: {acc:.2f}')
